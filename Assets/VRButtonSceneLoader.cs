2d2e7de12127cdc8d7e96bb45b5f030da2857120
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2025-01-30 12:11:11 +0000

    buttonworks

--- Assets/VRButtonSceneLoader.cs
+++ Assets/VRButtonSceneLoader.cs
@@ -1,18 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class VRButtonSceneLoader : MonoBehaviour
 {
     [Tooltip("The name of the scene to load when the button is pressed.")]
     public string sceneToLoad;
 
-    // Called when the button is selected (via controller or interaction)
+    private InputDevice targetDevice;
+
+    void Start()
+    {
+        // Try to get the right-hand controller (Oculus, OpenXR, etc.)
+        InitializeXRDevice();
+    }
+
+    void Update()
+    {
+        if (targetDevice.isValid)
+        {
+            bool buttonPressed = false;
+
+            // Check if the "A" button is pressed (CommonUsages.primaryButton usually corresponds to the "A" button)
+            if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out buttonPressed) && buttonPressed)
+            {
+                LoadScene();
+            }
+        }
+        else
+        {
+            // If device is not valid, try to reinitialize it (useful if a controller gets reconnected)
+            InitializeXRDevice();
+        }
+    }
+
+    void InitializeXRDevice()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    }
+
     public void LoadScene()
     {
         if (!string.IsNullOrEmpty(sceneToLoad))
         {
-            SceneManager.LoadScene(sceneToLoad); // Load the specified scene
+            SceneManager.LoadScene(sceneToLoad);
         }
         else
         {
